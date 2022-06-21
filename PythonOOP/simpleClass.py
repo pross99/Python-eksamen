39c6c43f4c751055a1e8dbaf70aa0fa4f4440d7a
Peter Ross <pete46m9@kea.stud.dk>
2022-06-21 22:29:45 +0200

    yes

--- PythonOOP/simpleClass.py
+++ PythonOOP/simpleClass.py
@@ -1,4 +1,9 @@
-class Employee:
+class Company:
+    pass
+
+class Employee(Company):
+
+    raise_amount = 1.04 #Class variable
 
     def __init__(self, first, last, pay):
         self.first = first
@@ -9,6 +14,15 @@ class Employee:
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+
 emp_1 = Employee('Peter', 'Ross', 50000)
 emp_2 = Employee('Test', 'Employee', 60000)
+
+
+print(Employee.raise_amount)
+print(emp_1.raise_amount)
+print(emp_2.raise_amount)
     
